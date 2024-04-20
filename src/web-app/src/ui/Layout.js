175969238be22c4570eb1fd425afb0ffd19ab4b5
hamzajg <hamzajg@gmail.com>
2024-04-20 04:49:41 +0100

    Refactoring.

--- src/web-app/src/ui/Layout.js
+++ src/web-app/src/ui/Layout.js
@@ -4,8 +4,9 @@ import {NavLink, useLocation} from "react-router-dom";
 const Layout = ({ children }) => {
     const location = useLocation();
     const segments = location.pathname.split('/');
+    const segmentsPath = location.pathname.split('/');
     const path = segments.pop();
-    const id = 'board' === path ? location.pathname.split('/')[location.pathname.split('/').length - 4] : ['generate', 'diagrams'].includes(path) ? location.pathname.split('/')[location.pathname.split('/').length - 2] : undefined;
+    const id = 'board' === path ? segmentsPath[segmentsPath.length - 4] : ['generate', 'diagrams'].includes(path) ? segmentsPath[segmentsPath.length - 2] : undefined;
     const activeLinkClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
     const inactiveLinkClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
     return (
@@ -36,16 +37,14 @@ const Layout = ({ children }) => {
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button type="button"
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <NavLink to={(id ? "/projects/" + id + "/generate" : "/solutions/") }>Solution Generation Wizard</NavLink>
+                            <NavLink to={id ? "/projects/" + id + "/generate" : "/solutions"}>Solution Generation Wizard</NavLink>
                         </button>
                     </div>
                 </div>
             </nav>
 
-            {/* Page Content */}
             <main className="flex-grow bg-gray-100">{children}</main>
 
-            {/* Footer */}
             <footer className="bg-gray-800 text-white py-4 px-6 w-full">
                 <div className="mx-auto">
                     <p>Odeev Kata PoC App © Hamza Jguerim - 2024</p>
